6b28616baeb37ad5b1fc860b180a224501051c07
Chiragitr <chirag.guptaitr@gmail.com>
2018-05-29 12:43:27 +0530

    Excercise 12

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv	
+
+script, first, second, third = argv		
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+x = raw_input("what is your age?")
+print "your age is %r and input through command line are %r %r %r" %(x, first, second, third)
