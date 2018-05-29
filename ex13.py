bd19f7eec28c345f59515ebb1e769548bf8da6e7
Chiragitr <chirag.guptaitr@gmail.com>
2018-05-29 12:46:04 +0530

    Excercise 13

--- ex13.py
+++ ex13.py
@@ -8,3 +8,4 @@ print "Your second variable is:", second
 print "Your third variable is:", third
 x = raw_input("what is your age?")
 print "your age is %r and input through command line are %r %r %r" %(x, first, second, third)
+#comment added
