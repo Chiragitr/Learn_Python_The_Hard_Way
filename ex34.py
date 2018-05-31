1f910931ecaf95566b9e823ba66a1d63f406c2c5
Chiragitr <chirag.guptaitr@gmail.com>
2018-05-31 19:14:39 +0530

    Exercise 34

---
+++ ex34.py
@@ -0,0 +1,3 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "1.The animal at 1:%r \n2.The third animal:%r \n3.The third animal:%r \n4.The animal at 3:%r \n5.The fifth animal:%r \n6.The animal at 2:%r \n7.The sixth animal:%r \n8.The animal at 4:%r \n" % (animals[1], animals[2], animals[0], animals[3], animals[4], animals[2], animals[5], animals[4])
