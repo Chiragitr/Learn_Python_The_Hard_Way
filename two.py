9f46be42c474318a0fa0ac8fc6cbc75d8fed2097
Chiragitr <chirag.guptaitr@gmail.com>
2018-06-06 07:46:41 +0530

    main_function

---
+++ two.py
@@ -0,0 +1,11 @@
+# file two.py
+
+
+print("top-level in two.py")
+import one
+
+one.func()
+if __name__ == "__main__":
+    print("two.py is being run directly")
+else:
+    print("two.py is being imported into another module")
